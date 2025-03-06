cc32d155a26dd7ddc97b044186d9963c64d2afa8
yasuomaidana <yasuomaidana@gmail.com>
2025-03-05 20:36:07 -0600

    COUR-1891: Running kibana and elastic search

--- installing-elk/start.sh
+++ installing-elk/start.sh
@@ -11,11 +11,24 @@ sudo systemctl enable kibana.service
 ## Generating backup of the configuration files
 mv /etc/kibana/kibana.yml /etc/kibana/kibana.yml.bak
 mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.bak
+mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.bak
 
 ## Copying the configuration files
 mv /kibana.yml /etc/kibana/kibana.yml
 mv /filebeat.yml /etc/filebeat/filebeat.yml
+mv /elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 mv /nginx.conf /etc/logstash/conf.d/nginx.conf
+
+
+sudo chown root:kibana /etc/kibana/kibana.yml
+sudo chmod 660 /etc/kibana/kibana.yml
+sudo chmod 2750 /etc/kibana/kibana.yml
+sudo chmod 2750 /etc/kibana
+
+cd /etc && chmod 2750 kibana
+cd ..
+
+echo cat install_output.txt | grep "The generated password for the elastic built-in superuser is : "
 
 # Start the services
 sudo systemctl start elasticsearch.service 

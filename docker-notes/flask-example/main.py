cbaf8df1ae8968cfb56c97b00528b5861c05601f
Yasuo Ignacio Maidana Perez <yasuo.maidana@epicor.com>
2025-11-14 11:42:58 -0600

    Add Flask application with environment variable greeting

---
+++ docker-notes/flask-example/main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def say_hello():
+    name = os.environ['Name']
+    return f'Hello, {name}!\n'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=8080)
